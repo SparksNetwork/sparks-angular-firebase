4828cc4e6dc2a66144fff21f5cf7950a0a4d6d57
ildikod <ildikod@imprezzio.com>
2017-08-02 01:14:10 +0300

    Refactor pipes location. Redundant code cleanup

--- functions/client/src/app/shared/snui/snui.module.ts
+++ functions/client/src/app/shared/snui/snui.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderSimpleComponent } from './header-simple/header-simple.component'
-import { LocationPipe } from "./pipes/location.pipe";
+import { LocationPipe } from "../pipes/location.pipe";
 import { HeaderCarouselComponent } from "./header-carousel/header-carousel.component";
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 
