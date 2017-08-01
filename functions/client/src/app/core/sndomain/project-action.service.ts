b504d011330f68bc636f6ee887b3013d7fab4013
sd <sdebaun74@gmail.com>
2017-08-01 01:37:55 -0700

    rest of CRUD on project-page and in service

--- functions/client/src/app/core/sndomain/project-action.service.ts
+++ functions/client/src/app/core/sndomain/project-action.service.ts
@@ -13,11 +13,49 @@ export class ProjectActionService {
 
   public create(value) {
     console.log('create', value)
-    this.http.post(APIROOT, value)
+    return this.http.post(APIROOT, value)
       .subscribe(data => {
         console.log('data', data.json())
       }, err => {
         console.log('err', err.json())
       })
+  }
+  
+  public replace(key: string, value) {
+    console.log('replace', value)
+    const url = `${APIROOT}/${key}`
+    console.log('url', url)
+    this.http.put(url, value)
+      .subscribe(data => {
+        console.log('data', data.json())
+      }, err => {
+        console.log('err', err.json())
+      })
+  }
+
+  public update(key: string, value) {
+    console.log('update', value)
+    const url = `${APIROOT}/${key}`
+    console.log('url', url)
+    this.http.patch(url, value)
+      .subscribe(data => {
+        console.log('data', data.json())
+      }, err => {
+        console.log('err', err.json())
+      })
+  }
+
+  public delete(key: string) {
+    console.log('delete', key)
+    const url = `${APIROOT}/${key}`
+    console.log('url', url)
+    const action = this.http.delete(url)
+    action
+      .subscribe(data => {
+        console.log('data', data.json())
+      }, err => {
+        console.log('err', err.json())
+      })
+    return action
   }
 }
--- functions/client/src/app/features/project/project-page/project-page.component.ts
+++ functions/client/src/app/features/project/project-page/project-page.component.ts
@@ -5,6 +5,7 @@ import { FormGroup, FormBuilder } from '@angular/forms'
 import { Router, ActivatedRoute } from '@angular/router'
 
 import { ProjectCollectionService } from '../../../core/sndomain/project-collection.service'
+import { ProjectActionService } from '../../../core/sndomain/project-action.service'
 
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/catch'
@@ -21,15 +22,13 @@ export class ProjectPageComponent implements OnInit {
   public editProject: FormGroup
 
   constructor(
-    // public af: AngularFireDatabase,
-    public http: Http,
     public projectCollection: ProjectCollectionService,
+    public projectAction: ProjectActionService,
     public builder: FormBuilder,
     public route: ActivatedRoute,
     public router: Router,
   ) {
     this.key = this.route.snapshot.paramMap.get('key')
-    console.log('key', this.key)
     this.project = projectCollection.one(this.key)
   }
 
@@ -41,39 +40,15 @@ export class ProjectPageComponent implements OnInit {
   }
 
   public replace() {
-    console.log('replace', this.editProject.value)
-    const url = `${APIROOT}/${this.key}`
-    console.log('url', url)
-    this.http.put(url, this.editProject.value)
-      .subscribe(data => {
-        console.log('data', data.json())
-      }, err => {
-        console.log('err', err.json())
-      })
+    this.projectAction.replace(this.key, this.editProject.value)
   }
 
   public update() {
-    console.log('update', this.editProject.value)
-    const url = `${APIROOT}/${this.key}`
-    console.log('url', url)
-    this.http.patch(url, this.editProject.value)
-      .subscribe(data => {
-        console.log('data', data.json())
-      }, err => {
-        console.log('err', err.json())
-      })
+    this.projectAction.update(this.key, this.editProject.value)
   }
 
   public delete() {
-    console.log('delete', this.editProject.value)
-    const url = `${APIROOT}/${this.key}`
-    console.log('url', url)
-    this.http.delete(url)
-      .subscribe(data => {
-        console.log('data', data.json())
-        this.router.navigate(['/project'])
-      }, err => {
-        console.log('err', err.json())
-      })
+    this.projectAction.delete(this.key)
+      .subscribe(data => this.router.navigate(['/project']))
   }
 }
