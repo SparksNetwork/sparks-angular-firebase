eb41be1fdc848f843f54bd50fc6e6351ea01c754
sd <sdebaun74@gmail.com>
2017-08-01 18:46:11 -0700

    clean unused code out of project-page

--- functions/client/src/app/features/project/project-page/project-page.component.ts
+++ functions/client/src/app/features/project/project-page/project-page.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { FirebaseObjectObservable } from 'angularfire2/database'
-import { FormGroup, FormBuilder } from '@angular/forms'
-import { Router, ActivatedRoute } from '@angular/router'
-
-import {
-  ProjectQueryService,
-  ProjectActionService,
-} from '../../../core/sndomain/project'
-
-
-import 'rxjs/add/operator/do'
-import 'rxjs/add/operator/catch'
+import { ActivatedRoute } from '@angular/router'
 
 import { IProject } from "../../../../../../shared/interfaces/project.model";
 
-const APIROOT = 'http://localhost:5002/sparks-development-sd/us-central1/api/project'
 @Component({
   selector: 'project-project-page',
   templateUrl: 'project-page.component.html'
@@ -23,13 +12,9 @@ const APIROOT = 'http://localhost:5002/sparks-development-sd/us-central1/api/pro
 export class ProjectPageComponent implements OnInit {
   public key: string
   public project: FirebaseObjectObservable<IProject>
-  public editProject: FormGroup
 
   constructor(
-    public projectAction: ProjectActionService,
-    public builder: FormBuilder,
     public route: ActivatedRoute,
-    public router: Router,
   ) {
     this.key = this.route.snapshot.paramMap.get('key')
     this.project = this.route.snapshot.data['sources']['project']
@@ -43,23 +28,4 @@ export class ProjectPageComponent implements OnInit {
     })
   }
 
-  public ngOnInit() {
-    this.editProject = this.builder.group({
-      name: '',
-      description: '',
-    })
-  }
-
-  public replace() {
-    this.projectAction.replace(this.key, this.editProject.value)
-  }
-
-  public update() {
-    this.projectAction.update(this.key, this.editProject.value)
-  }
-
-  public delete() {
-    this.projectAction.delete(this.key)
-      .subscribe(data => this.router.navigate(['/project']))
-  }
 }
