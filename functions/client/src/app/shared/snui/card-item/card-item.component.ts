b6c3a5c14a3c7da9efbe5ce09476df9ba07ae3ae
sd <sdebaun74@gmail.com>
2017-08-02 13:07:31 -0700

    port of project page multiple opp card display

--- functions/client/src/app/shared/snui/card-item/card-item.component.ts
+++ functions/client/src/app/shared/snui/card-item/card-item.component.ts
@@ -8,12 +8,10 @@ import { CardItemStatus } from "../../../../../../shared/enums/card-item-status.
 export class CardItemComponent implements OnInit {
 
   @Input() title: string;
+  @Input() subtitle: string;
   @Input() description: string;
   @Input() summary: string;
   @Input() icon: string;
-  @Input() status: CardItemStatus;
-
-  cardItemStatus = CardItemStatus;
 
   constructor() { }
 
