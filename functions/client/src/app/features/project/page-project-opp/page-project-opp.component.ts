8e329b8778be9a1dd871f1ffe510bc57b35aa1e0
sd <sdebaun74@gmail.com>
2017-08-05 10:37:24 -0700

    hacky temp implementation of discountDescription

--- functions/client/src/app/features/project/page-project-opp/page-project-opp.component.ts
+++ functions/client/src/app/features/project/page-project-opp/page-project-opp.component.ts
@@ -4,13 +4,15 @@ import { FirebaseListObservable, FirebaseObjectObservable } from "angularfire2/d
 import { ICard } from "../../../core/interfaces/card.model";
 import { IOpportunityCard } from "../../../core/interfaces/opportunity-card.model";
 
+import { Observable } from 'rxjs'
+
 @Component({
   selector: 'project-page-project-opp',
   templateUrl: './page-project-opp.component.html'
 })
 export class PageProjectOppComponent implements OnInit {
-  private opps: FirebaseListObservable<any[]>
-  private opp: FirebaseObjectObservable<any>
+  private opps: Observable<any[]>
+  private opp: Observable<any>
 
   constructor(
     public route: ActivatedRoute
@@ -20,7 +22,10 @@ export class PageProjectOppComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.opp = this.route.snapshot.data['opp']
-      this.opps = this.route.snapshot.data['opps']
+        .map(opp => {opp.discountDescription = 'bar'; return opp})
+      this.opps = (this.route.snapshot.data['opps'] as FirebaseListObservable<any>)
+        .map(opps => opps.map(opp => {opp.discountDescription = 'foo'; return opp}))
+
     })
   }
 }
