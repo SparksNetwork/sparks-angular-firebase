035b7ab81e37af53e7fecead0a74fb1549035adf
sd <sdebaun74@gmail.com>
2017-08-06 22:41:26 -0700

    add firebase config to prod environment for build

--- functions/client/src/environments/environment.prod.ts
+++ functions/client/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  /*   firebase: {
+      apiKey: 'AIzaSyAd6U9h9zzhm8RO17-O05IQeeMmZbcJ7oc',
+      authDomain: 'sparks-development-sd.firebaseapp.com',
+      databaseURL: 'https://sparks-development-sd.firebaseio.com',
+      storageBucket: 'gs://sparks-development-sd.appspot.com',
+      // messagingSenderId: '277847429817',
+    }, */
+  firebase: {
+    apiKey: "AIzaSyCQXrtWwVgGjZ0dOo5W92HnFvkWl9kgns4",
+    authDomain: "sparksnetwork-6de8b.firebaseapp.com",
+    databaseURL: "https://sparksnetwork-6de8b.firebaseio.com",
+    projectId: "sparksnetwork-6de8b",
+    storageBucket: "sparksnetwork-6de8b.appspot.com",
+    messagingSenderId: "683448204097"
+  }
 };
