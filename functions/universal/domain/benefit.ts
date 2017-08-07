9af460f082a7e68e2601bb9b7d47f3b7e34d9a34
ildikod <ildikod@imprezzio.com>
2017-08-07 15:47:13 +0300

    Add Benefit model class and validation

--- functions/universal/domain/benefit.ts
+++ functions/universal/domain/benefit.ts
@@ -1,13 +1,42 @@
+import { IsNotEmpty, IsEnum } from 'class-validator/decorator/decorators'
+import { transformAndValidate } from "class-transformer-validator";
+import { Expose } from "class-transformer";
+
 import {
   BasePaths,
   BaseCollection,
 } from '../../lib/firebase-universal/shared'
 
+
 export class BenefitPaths extends BasePaths {
   firebase = '/benefit'
   api = 'http://localhost:5002/sparks-development-sd/us-central1/api/benefit'
-  // firebase = '/Projects'
-  // api = 'https://sparksnetwork-6de8b.firebaseio.com/Projects'
+}
+
+export enum BenefitType { 
+   FoodTicket,
+   EventTicket,
+   Gifts,
+   FoodDrink,
+   HelpNonProfit
+}
+
+export class Benefit {
+    @IsNotEmpty()
+    @Expose()
+    public $key: string
+
+    @IsNotEmpty()
+    oppKey: string;
+
+    @IsEnum(BenefitType)
+    type: BenefitType;
+
+    title: string;
+
+    description: string;
+
+    icon?: string;
 }
 
 // any methods here will be available on both client and server
@@ -16,3 +45,9 @@ export class BenefitCollection extends BaseCollection {
     return this.by('oppKey', key)
   }
 }
+
+const validateOpt = { validator: { skipMissingProperties: true } };
+
+// we have two transform functions for type safety, not sure why overloading isnt working see below
+export const benefitTransform = (input: object) => transformAndValidate<Benefit>(Benefit, input, validateOpt)
+export const benefitsTransform = (input: object[]) => transformAndValidate<Benefit>(Benefit, input, validateOpt)
