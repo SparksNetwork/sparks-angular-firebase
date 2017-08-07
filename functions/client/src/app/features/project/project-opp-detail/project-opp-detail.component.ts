f44ea9b6db8bf1cbe6d7e3cb2ce5da58525dd38f
Szabo Tamas <tamass@imprezzio.com>
2017-08-07 14:09:33 +0300

    loaded project, opp from route  data

--- functions/client/src/app/features/project/project-opp-detail/project-opp-detail.component.ts
+++ functions/client/src/app/features/project/project-opp-detail/project-opp-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { Observable } from 'rxjs';
+import { Project } from "../../../../../../universal/domain/project";
+import { Opp } from "../../../../../../universal/domain/opp";
 
 @Component({
     selector: 'project-opp-detail',
@@ -6,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProjectOppDetailComponent implements OnInit {
-    constructor() { }
+    public project: Observable<Project>;
+    public opp: Observable<Opp>;
+
+    constructor(
+        public route: ActivatedRoute
+    ) { }
 
     ngOnInit() {
-        console.log('loaded')
-     }
+        this.route.data.subscribe(data => {
+            this.project = data['project'];
+            this.project.subscribe((s) => { console.log(s) });
+            this.opp = data['opp'];
+            this.opp.subscribe((s) => { console.log(s) });
+        });
+    }
 }
