7985551c8318dc9e543485eab2b7136431df5a81
Szabo Tamas <tamass@imprezzio.com>
2017-08-07 12:24:01 +0300

    removed any, added strongly type for observables

--- functions/client/src/app/features/project/page-project-opp/page-project-opp.component.ts
+++ functions/client/src/app/features/project/page-project-opp/page-project-opp.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
 import { Observable } from 'rxjs'
+import { Opp } from "../../../../../../universal/domain/opp";
 
 @Component({
   selector: 'project-page-project-opp',
   templateUrl: './page-project-opp.component.html'
 })
 export class PageProjectOppComponent implements OnInit {
-  private opps: Observable<any[]>
-  private opp: Observable<any>
+  private opps: Observable<Opp[]>
+  private opp: Observable<Opp>
 
   constructor(
     public route: ActivatedRoute
