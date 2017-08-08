6a46289c96e677efa9885ecf5f0255c9009866ba
crinelap <crinelap@imprezzio.com>
2017-08-08 17:47:41 +0300

    Tests for project page url validation

---
+++ functions/e2e/project/specifications/project.projectPageUrl.e2e-spec.ts
@@ -0,0 +1,53 @@
+import { browser } from "protractor/built";
+import { ProjectPage } from "../project.po";
+var firebaseAccessHandler = require('../../setup/firebaseAccess');
+
+describe('Testing the model validation attributes for projectPageUrl', () => {
+    let page: ProjectPage;
+
+    beforeEach(() => {
+        page = new ProjectPage();
+    });
+
+    afterEach(() => {
+        firebaseAccessHandler.deleteLoadedData("project", 1, 1);
+        browser.sleep(500);
+    });
+
+    it('Test projectPageUrl @IsUrl() ', () => {
+        firebaseAccessHandler.loadFile("./e2e/project/testDataFiles/projectPageUrlNotUrl.json", "project");
+        browser.sleep(500);
+        page.navigateTo();
+        browser.sleep(1000);
+
+        browser.manage().logs().get('browser').then(function (browserLogs) {
+
+            let isValidationErrorsStringPresent = false;
+            let isPropertyStringPresent = false;
+            let isProjectPageUrlValid= false;
+
+            browserLogs.forEach(function (log) {
+                if (log.message.includes("Validation Errors:")) {
+                    isValidationErrorsStringPresent = true;
+                }
+                if (log.message.includes("property")) {
+                    isPropertyStringPresent = true;
+                }
+                if (log.message.includes("projectPageUrl must be an URL address")) {
+                    isProjectPageUrlValid = true;
+                }
+
+            });
+
+            expect(isValidationErrorsStringPresent)
+                .toBe(true, "String `Validation Errors:` was not present");;
+            expect(isPropertyStringPresent)
+                .toBe(true, "String `property` was not present");
+            expect(isProjectPageUrlValid)
+                .toBe(true, "String `projectPageUrl must be an URL address` was not present");
+           
+        });
+
+    });
+
+})
