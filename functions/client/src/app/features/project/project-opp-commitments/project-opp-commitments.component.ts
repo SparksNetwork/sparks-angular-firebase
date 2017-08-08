73bf08466a01d63b301a0bfbcb6d576a15411bbb
Szabo Tamas <tamass@imprezzio.com>
2017-08-08 14:50:06 +0300

    generate opportunity commitments component

---
+++ functions/client/src/app/features/project/project-opp-commitments/project-opp-commitments.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'project-opp-commitments',
+    templateUrl: 'project-opp-commitments.component.html'
+})
+
+export class ProjectOppCommitmentsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
