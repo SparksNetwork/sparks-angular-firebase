4a3a49c1823e67ab791ab963c166def86e189687
crinelap <crinelap@imprezzio.com>
2017-08-09 12:37:35 +0300

    Test for image nested validation

--- functions/e2e/project/specifications/project.images.e2e-spec.ts
+++ functions/e2e/project/specifications/project.images.e2e-spec.ts
@@ -15,10 +15,33 @@ describe('Testing the model validation attributes for images', () => {
     });
 
 
-    //validation logic not implemented yet
-    // it('Test images @IsUrl() ', () => {
+    it('Test images @ValidateNested() ', () => {
+        firebaseAccessHandler.loadFile("./e2e/project/testDataFiles/imagesNotNested.json", "project");
+        browser.sleep(500);
+        page.navigateTo();
+        browser.sleep(1000);
 
-    // });
+        browser.manage().logs().get('browser').then(function (browserLogs) {
 
+            let isValidationErrorsStringPresent = false;
+            let isImagesNestedStringPresent = false;
+
+            browserLogs.forEach(function (log) {
+                if (log.message.includes("Validation Errors:")) {
+                    isValidationErrorsStringPresent = true;
+                }
+                if (log.message.includes("Only objects and arrays are supported to nested validation")) {
+                    isImagesNestedStringPresent = true;
+                }
+            });
+
+            expect(isValidationErrorsStringPresent)
+                .toBe(true, "String `Validation Errors:` was not present");;
+            expect(isImagesNestedStringPresent)
+                .toBe(true, "String `Only objects and arrays are supported to nested validation` was not present");
+           
+        });
+
+    });
 
 })
