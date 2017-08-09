bbdee73d038f43d3c410ea48abfcfc6439df5778
ildikod <ildikod@imprezzio.com>
2017-08-09 14:55:03 +0300

    Set up 'id' dev environment

---
+++ functions/client/src/environments/environment.dev-id.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseAdminCredentialFilename: 'firebaseAdminCredentials.dev-id.json',
+  firebase: {
+    apiKey: "AIzaSyDzZA0hgq1ch38x2QsdpzyeUpOMFkHdg_0",
+    authDomain: "sparksnetworkildi.firebaseapp.com",
+    databaseURL: "https://sparksnetworkildi.firebaseio.com",
+    projectId: "sparksnetworkildi",
+    storageBucket: "",
+    messagingSenderId: "897039920285"
+  }
+};
