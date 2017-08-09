5e7f0c6ca0aaebf7a25c9e34fc60d565a938423a
Szabo Tamas <tamass@imprezzio.com>
2017-08-09 12:59:12 +0300

    generate project card component

---
+++ functions/client/src/app/shared/snui/project-card-item/project-card-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'snui-project-card-item',
+    templateUrl: 'project-card-item.component.html'
+})
+
+export class ProjectCardItemComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
