9dce2907f8c2c0f9941306202576f3b6317ed6e0
Szabo Tamas <tamass@imprezzio.com>
2017-08-09 13:42:29 +0300

    generated components for displaying projects

---
+++ functions/client/src/app/features/home/home-all-projects/home-all-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'home-all-projects',
+    templateUrl: 'home-all-projects.component.html'
+})
+
+export class HomeAllProjectsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
