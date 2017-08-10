ed41a48e56a09e71c9e7a79f5179fdb95095b708
Szabo Tamas <tamass@imprezzio.com>
2017-08-10 12:42:27 +0300

    loaded teams from route data and iterate them in template

--- functions/client/src/app/features/apply/page-opp-teams/page-opp-teams.component.ts
+++ functions/client/src/app/features/apply/page-opp-teams/page-opp-teams.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Rx";
+import { Team } from "../../../../../../universal/domain/team";
 
 @Component({
     templateUrl: 'page-opp-teams.component.html'
 })
 
 export class PageOppTeamsComponent implements OnInit {
-    constructor() { }
+    public teams: Observable<Team[]>;
 
-    ngOnInit() { }
+    constructor(
+        public route: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+        this.route.data.subscribe(data => {
+            this.teams = data['teams'];
+        })
+    }
 }
