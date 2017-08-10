84e9f601fda4bb767c5096726888d9a0a4ede479
Szabo Tamas <tamass@imprezzio.com>
2017-08-10 12:26:41 +0300

    generate shared team card component

---
+++ functions/client/src/app/shared/snui/team-card-item/team-card-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'snui-team-card-item',
+    templateUrl: 'team-card-item.component.html'
+})
+
+export class TeamCardItemComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
