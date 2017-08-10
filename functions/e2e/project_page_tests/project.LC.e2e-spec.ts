d3d1f8393bce0b25b178d1c0aba5f03082fd34e6
crinelap <crinelap@imprezzio.com>
2017-08-10 16:47:13 +0300

    Testing LC Oportunities

--- functions/e2e/project_page_tests/project.LC.e2e-spec.ts
+++ functions/e2e/project_page_tests/project.LC.e2e-spec.ts
@@ -8,10 +8,18 @@ describe("Project LC tests", () => {
 
     beforeAll(() => {
         firebaseAccessHandler.loadFile('./e2e/project_page_tests/test_files/LCOriginal.json', "project");
+        firebaseAccessHandler.loadFile('./e2e/project_page_tests/test_files/LCOriginalOpp.json', "opp");
+
     });
 
     afterAll(() => {
         firebaseAccessHandler.deleteItemByKey("project", "LCTest");
+        firebaseAccessHandler.deleteItemByKey("opp", "LCTest1");
+        firebaseAccessHandler.deleteItemByKey("opp", "LCTest2");
+        firebaseAccessHandler.deleteItemByKey("opp", "LCTest3");
+        firebaseAccessHandler.deleteItemByKey("opp", "LCTest4");
+        firebaseAccessHandler.deleteItemByKey("opp", "LCTest5");
+
     });
 
     beforeEach(() => {
@@ -26,7 +34,6 @@ describe("Project LC tests", () => {
         page.getProjectTitle().then(function (str) { expect(str).toBe("Lucidity Crossroads") });
 
     });
-
 
     it('Navbar-Brand link should be `sparks.network`', function () {
 
@@ -47,7 +54,87 @@ describe("Project LC tests", () => {
         page.getCarouselActiveImageDiv().getAttribute('style')
             .then(function (str) { expect(str).toContain('https://placeimg.com/1140/410/animals/grayscale') });
 
+    });
 
+    it('It should display 5 oportunity links', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+        page.getNumberOfOportunityLinks().then(function (str) { expect(str).toBe(5) });
+
+    });
+
+    it('It should display an specific icon for each oportunity ', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getFirstOportunitySpan().getAttribute('class')
+            .then(function (str) { expect(str).toContain('glyphicon-music') });
+
+        page.getSecondOportunitySpan().getAttribute('class')
+            .then(function (str) { expect(str).toContain("glyphicon-glass") });
+            
+        page.getThirdOportunitySpan().getAttribute('class')
+            .then(function (str) { expect(str).toContain("glyphicon-film") });
+            
+        page.getFourthOportunitySpan().getAttribute('class')
+            .then(function (str) { expect(str).toContain("glyphicon-camera") });
+            
+        page.getFifthOportunitySpan().getAttribute('class')
+            .then(function (str) { expect(str).toContain("glyphicon-cog") });
+    });
+
+    it('First oportunity should have the title: 1-Shift $209', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getFirstOportunityTitle()
+            .then(function (str) { expect(str).toEqual('1-Shift $209') });
+    });
+
+    it('First oportunity should have the contribution value: $374 in value', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getFirstOportunityContribValue()
+            .then(function (str) { expect(str).toEqual('$374 in value') });
+    });
+
+    it('First oportunity should have a 44% discount', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getFirstOportunityDiscount()
+            .then(function (str) { expect(str).toEqual('44% discount') });
+    });
+
+    it('Third oportunity should have the title: Pre-Event Weekends', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getThirdOportunityTitle()
+            .then(function (str) { expect(str).toEqual('Pre-Event Weekends') });
+    });
+
+    it('Third oportunity should have the contribution value: $364 in value', function () {
+
+        page.navigateTo();
+        browser.sleep(3000);
+        browser.waitForAngularEnabled(false);
+
+        page.getThirdOportunityContribValue()
+            .then(function (str) { expect(str).toEqual('$364 in value') });
     });
 
 
--- functions/e2e/project_page_tests/project.po.ts
+++ functions/e2e/project_page_tests/project.po.ts
@@ -10,17 +10,70 @@ export class ProjectPage {
     return element(by.css('h1.event-title')).getText();
   }
 
-  getNavbarBrandLink(){
+  getNavbarBrandLink() {
     return element(by.css('a.navbar-brand')).getText();
   }
 
-  getLastCarouselIndicator(){
+  getLastCarouselIndicator() {
     return element.all(by.css('ol.carousel-indicators li')).last();
-  } 
-
-  getCarouselActiveImageDiv(){
-    return element(by.className('item carousel-item active'))
-    .element(by.css('div.item.active'))
-    .element(by.css('div.item'));
   }
+
+  getCarouselActiveImageDiv() {
+    return element(by.className('item carousel-item active'))
+      .element(by.css('div.item.active'))
+      .element(by.css('div.item'));
+  }
+
+  getNumberOfOportunityLinks() {
+    return element.all(by.css('snui-card-item')).count();
+  }
+
+  getFirstOportunitySpan() {
+    return element.all(by.css('snui-card-item')).first()
+      .$$('div.icon').first().$('span');
+  }
+  getSecondOportunitySpan() {
+    return element.all(by.css('snui-card-item')).get(1)
+      .$$('div.icon').first().$('span');
+  }
+  getThirdOportunitySpan() {
+    return element.all(by.css('snui-card-item')).get(2)
+      .$$('div.icon').first().$('span');
+  }
+  getFourthOportunitySpan() {
+    return element.all(by.css('snui-card-item')).get(3)
+      .$$('div.icon').first().$('span');
+  }
+  getFifthOportunitySpan() {
+    return element.all(by.css('snui-card-item')).get(4)
+      .$$('div.icon').first().$('span');
+  }
+
+  getFirstOportunityTitle() {
+    return element.all(by.css('snui-card-item')).first()
+      .$$('div.text').first().$('h4').getText();
+  }
+
+  getFirstOportunityContribValue() {
+    return element.all(by.css('snui-card-item')).first()
+      .$$('div.text').first().all(by.css('p')).first().getText();
+  }
+
+  getFirstOportunityDiscount() {
+    return element.all(by.css('snui-card-item')).first()
+      .$$('div.text').first().all(by.css('p')).last().getText();
+  }
+
+  getThirdOportunityTitle() {
+    return element.all(by.css('snui-card-item')).get(2)
+      .$$('div.text').first().$('h4').getText();
+  }
+
+  getThirdOportunityContribValue() {
+    return element.all(by.css('snui-card-item')).get(2)
+      .$$('div.text').first().all(by.css('p')).first().getText();
+  }
+
+
+
 }
