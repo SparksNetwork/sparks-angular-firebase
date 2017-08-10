739c36506915923748d3a50e73375dae85dc1da3
Szabo Tamas <tamass@imprezzio.com>
2017-08-10 16:12:25 +0300

    generated opp team component

---
+++ functions/client/src/app/features/apply/page-opp-team/page-opp-team.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'page-opp-team.component.html'
+})
+
+export class PageOppTeamComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
