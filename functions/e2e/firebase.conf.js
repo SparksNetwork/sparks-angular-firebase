af6e1719d784a41539e8162b161397694c415a57
ildikod <ildikod@imprezzio.com>
2017-08-10 13:49:31 +0300

    Remove redundant config file and example json

--- functions/e2e/firebase.conf.js
+++
@@ -1,8 +0,0 @@
-exports.firebaseConfig= {
-     apiKey: "AIzaSyBlSVwhkyWAJG2HAJb1cucEQzSiYH1t-gE",
-    authDomain: "sparksnetworktest.firebaseapp.com",
-    databaseURL: "https://sparksnetworktest.firebaseio.com",
-    projectId: "sparksnetworktest",
-    storageBucket: "sparksnetworktest.appspot.com",
-    messagingSenderId: "529336251324"
-};
