cb31c8da404c8e8d72dca9340ce81c5d2bfc962a
Szabo Tamas <tamass@imprezzio.com>
2017-08-11 16:40:09 +0300

    refactor: extract displaying selected teams in a component

--- functions/client/src/app/features/apply/page-opp-teams/page-opp-teams.component.ts
+++ functions/client/src/app/features/apply/page-opp-teams/page-opp-teams.component.ts
@@ -11,28 +11,16 @@ import { OppTeamsSelectService } from "../opp-teams-select.service";
 })
 
 export class PageOppTeamsComponent implements OnInit {
-    private _teams: Observable<Team[]>;
+    public teams: Observable<Team[]>;
     public actionBarType = ActionBarType;
-    public selectedTeams: Team[];
-    public notSelectedTeams: Team[];
 
     constructor(
-        public route: ActivatedRoute,
-        private oppTeamsSelectService: OppTeamsSelectService
+        public route: ActivatedRoute
     ) { }
 
     ngOnInit() {
         this.route.data.subscribe(data => {
-            this._teams = data['teams'];
-            let selectedKeys = this.oppTeamsSelectService.getTeamKeys();
-            if (this._teams) {
-                this._teams.subscribe(
-                    (teams: Team[]) => {
-                        this.selectedTeams = teams.filter(t => selectedKeys.indexOf(t.$key) > -1);
-                        this.notSelectedTeams = teams.filter(t => selectedKeys.indexOf(t.$key) === -1)
-                    }
-                )
-            }
+            this.teams = data['teams'];
         })
     }
 }
