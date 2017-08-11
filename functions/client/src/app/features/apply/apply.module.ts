4f22a8c3fd3b8063900ac531187c63f809a4f40f
ildikod <ildikod@imprezzio.com>
2017-08-11 10:49:50 +0300

    Add complete profile form component

--- functions/client/src/app/features/apply/apply.module.ts
+++ functions/client/src/app/features/apply/apply.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SNUIModule } from "../../shared/snui/snui.module";
 import { ApplyRoutingModule, routedComponents } from "./apply-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormCompleteProfileComponent } from "./form-complete-profile/form-complete-profile.component";
 
 @NgModule({
-  declarations: routedComponents,
+  declarations: [
+    routedComponents,
+    FormCompleteProfileComponent
+  ],
   imports: [
     CommonModule,
     SNUIModule,
     ApplyRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 })
 export class ApplyModule { }
