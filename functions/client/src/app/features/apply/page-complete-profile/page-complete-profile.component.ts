808585d59b761075e7dbffa51a135a49ed01c589
sd <sdebaun74@gmail.com>
2017-08-14 12:13:17 -0700

    connect complete profile form to component, disable button if not valid

--- functions/client/src/app/features/apply/page-complete-profile/page-complete-profile.component.ts
+++ functions/client/src/app/features/apply/page-complete-profile/page-complete-profile.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { FormCompleteProfileComponent } from '../form-complete-profile/form-complete-profile.component'
 
 @Component({
   selector: 'apply-page-complete-profile',
   templateUrl: './page-complete-profile.component.html',
 })
-export class PageCompleteProfileComponent implements OnInit {
+export class PageCompleteProfileComponent {
+  @ViewChild(FormCompleteProfileComponent) public profForm: FormCompleteProfileComponent
 
   constructor() { }
 
-  ngOnInit() {
+  public next() {
+    console.log('completed profile?', this.profForm.profileForm.value)
   }
-
 }
