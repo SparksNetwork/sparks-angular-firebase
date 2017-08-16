970901361cd8a63472a94f112df220382788300f
Szabo Tamas <tamass@imprezzio.com>
2017-08-16 13:53:33 +0300

    implement remove all

--- functions/client/src/app/features/apply/opp-teams-selected-remove/opp-teams-selected-remove.component.ts
+++ functions/client/src/app/features/apply/opp-teams-selected-remove/opp-teams-selected-remove.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OppTeamsSelectService } from "../opp-teams-select.service";
 
 @Component({
     template:``
 })
 
 export class OppTeamsSelectedRemoveComponent implements OnInit {
-    constructor() { }
+
+    constructor(
+        private oppTeamsSelectService: OppTeamsSelectService,
+        private router: Router,
+        private route: ActivatedRoute,) { }
 
     ngOnInit() { 
-        console.log('loaded');
+       //if there is no key, remove all
+       this.oppTeamsSelectService.removeAllTeamKeys();
+       this.router.navigate(['../'], {relativeTo: this.route}) ;   
     }
 }
