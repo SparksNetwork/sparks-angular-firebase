b2a8b5529a95cda7334f1355b536152ad21bfa34
Szabo Tamas <tamass@imprezzio.com>
2017-08-18 11:32:40 +0300

    added resolver and domain class

--- functions/client/src/app/core/sndomain/applicationTeam/application-team-query.service.ts
+++ functions/client/src/app/core/sndomain/applicationTeam/application-team-query.service.ts
@@ -1,9 +1,12 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
+import { ApplicationTeamCollection } from "../../../../../../universal/domain/applicationTeam";
 
 @Injectable()
-export class ApplicationTeamQueryService {
-    public vals: object[]
-    public emitter: EventEmitter<object[]>
-
-
+export class ApplicationTeamQueryService extends ApplicationTeamCollection {
+    constructor(
+        public af: AngularFireDatabase
+      ) {
+        super(af.database)
+      }
 }
