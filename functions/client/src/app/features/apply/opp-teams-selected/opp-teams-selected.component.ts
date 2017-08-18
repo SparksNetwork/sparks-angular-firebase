1d6c7775753cb9ec9b4141863eb7b38f536061a0
Szabo Tamas <tamass@imprezzio.com>
2017-08-18 15:53:03 +0300

    call api for removing selected team

--- functions/client/src/app/features/apply/opp-teams-selected/opp-teams-selected.component.ts
+++ functions/client/src/app/features/apply/opp-teams-selected/opp-teams-selected.component.ts
@@ -34,4 +34,8 @@ export class OppTeamsSelectedComponent implements OnChanges {
             }
         }
     }
+
+    delete(key: string) {
+        this.applicationTeamAction.delete(key).subscribe();
+    }
 }
