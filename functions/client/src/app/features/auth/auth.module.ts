94b7cde551aa65efa61be1ae4ea71675224b950c
Emanuel Virca <emanuelv@imprezzio.com>
2017-08-18 10:30:08 +0300

    remove button-google-signin component and use it inline

--- functions/client/src/app/features/auth/auth.module.ts
+++ functions/client/src/app/features/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { SNUIModule } from '../../shared/snui/snui.module'
 import { AuthRoutingModule, routedComponents } from './auth-routing.module'
 
 import { FormEmailPasswordComponent } from './form-email-password/form-email-password.component'
-import { ButtonSigninGoogleComponent } from './button-signin-google/button-signin-google.component'
 import { AlertErrorComponent } from './alert-error/alert-error.component'
 import { FormResetPasswordComponent } from './form-reset-password/form-reset-password.component'
 
@@ -14,7 +13,6 @@ import { FormResetPasswordComponent } from './form-reset-password/form-reset-pas
   declarations: [
     routedComponents,
     FormEmailPasswordComponent,
-    ButtonSigninGoogleComponent,
     AlertErrorComponent,
     FormResetPasswordComponent,
   ],
--- functions/client/src/app/features/auth/page-signin/page-signin.component.ts
+++ functions/client/src/app/features/auth/page-signin/page-signin.component.ts
@@ -12,4 +12,8 @@ export class PageSigninComponent {
   signInWithFacebook() {
     this.auth.signInWithFacebook()
   }
+
+  signInWithGoogle() {
+    this.auth.signInWithGoogle()
+  }
 }
