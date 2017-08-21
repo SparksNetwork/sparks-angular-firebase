bf9a01819914d68778f385be18b0ed1a7740a3b6
ildikod <ildikod@imprezzio.com>
2017-08-21 10:38:58 +0300

    Profile model validation

--- functions/client/src/app/core/sndomain/profile/profile-query.service.ts
+++ functions/client/src/app/core/sndomain/profile/profile-query.service.ts
@@ -5,23 +5,25 @@ import { Observable } from 'rxjs'
 import 'rxjs/add/operator/switchMap'
 
 import { AuthService } from '../../snauth/auth/auth.service'
-import { Profile, ProfileCollection } from '../../../../../../universal/domain/profile'
+import { Profile, ProfileCollection, profileTransform } from '../../../../../../universal/domain/profile'
 
 import { obj } from '../../../../../../lib/firebase-angular-observables'
+import { SorryService } from "../../sorry/index";
 
 @Injectable()
 export class ProfileQueryService extends ProfileCollection {
-  public current: Observable<Profile>
+  public current: Observable<void | Profile>
 
   constructor(
     public af: AngularFireDatabase,
     public auth: AuthService,
+    public sorry: SorryService,
   ) {
     super(af.database)
     this.current = this.auth.current
       .switchMap(user => {
         if (user && user.uid) {
-          return obj(this.one(user.uid))
+          return obj(this.one(user.uid)).switchMap(this.sorry.intercept(profileTransform))
         } else {
           return Observable.of({})
         }
--- functions/universal/domain/profile.ts
+++ functions/universal/domain/profile.ts
@@ -1,6 +1,6 @@
 import { transformAndValidate } from 'class-transformer-validator'
 import { Expose } from 'class-transformer'
-// import { IsNotEmpty, IsEnum, ValidationError, IsDateString, ValidateNested, IsNumber, IsInt, IsUrl, IsDefined } from 'class-validator'
+import { IsISO8601, IsMobilePhone } from "class-validator";
 
 import {
   BaseCollection,
@@ -12,6 +12,7 @@ import {
 // import { ImageRef } from './imageRef'
 // import { Organizer } from './organizer'
 import { logErrors } from '../logger/logger'
+
 
 // any methods here will be available on both client and server
 export class ProfileCollection extends BaseCollection {
@@ -26,54 +27,12 @@ export class ProfileCollection extends BaseCollection {
 export class Profile {
   legalName: string
   preferredName: string
+
+  @IsMobilePhone('en-US')
   phoneNumber: string
+
+  @IsISO8601()
   birthday: string
-  // @Expose()
-  // @IsDefined()
-  // @IsNotEmpty()
-  // public $key: string;
-
-  // @IsEnum(ProjectType)
-  // projectType: ProjectType;
-
-  // @IsDefined()
-  // @IsNotEmpty()
-  // title: string;
-
-  // @IsDefined()
-  // @IsNotEmpty()
-  // description: string;
-
-  // @IsDefined()
-  // @IsDateString()
-  // startDateTime: string;
-
-  // @IsDateString()
-  // endDateTime?: string;
-
-  // @IsDefined()
-  // @ValidateNested()
-  // location: Location;
-
-  // @ValidateNested()
-  // images: ImageRef[];
-
-  // @IsNumber()
-  // ticketPrice?: number;
-
-  // @IsDefined()
-  // @IsInt()
-  // maxKarmaPoints: number;
-
-  // @IsDefined()
-  // @ValidateNested()
-  // organizer: Organizer;
-
-  // @IsUrl()
-  // projectPageUrl?: string;
-
-  // @IsInt()
-  // shareKarmaPoints?: number;
 }
 
 const validateOpt = { validator: { skipMissingProperties: true } };
