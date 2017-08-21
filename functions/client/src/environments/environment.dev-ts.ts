94c2809b00a2585b29addffbb4bfc0ad02c8a20c
Szabo Tamas <tamass@imprezzio.com>
2017-08-21 09:25:07 +0300

    setup local env

---
+++ functions/client/src/environments/environment.dev-ts.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseAdminCredentialFilename: '',
+    apiRoot: 'http://localhost:5000/sparksnetwork-6de8b/us-central1/api',
+    firebase: {
+      apiKey: "AIzaSyCQXrtWwVgGjZ0dOo5W92HnFvkWl9kgns4",
+      authDomain: "sparksnetwork-6de8b.firebaseapp.com",
+      databaseURL: "https://sparksnetwork-6de8b.firebaseio.com",
+      projectId: "sparksnetwork-6de8b",
+      storageBucket: "sparksnetwork-6de8b.appspot.com",
+      messagingSenderId: "683448204097"
+    }
+  };
