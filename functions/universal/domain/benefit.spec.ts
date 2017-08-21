c66f839886547475e79367204cdf62e8e13d76d1
crinelap <crinelap@imprezzio.com>
2017-08-21 16:28:44 +0300

    Benefit and contrib Unit tests

--- functions/universal/domain/benefit.spec.ts
+++ functions/universal/domain/benefit.spec.ts
@@ -6,6 +6,7 @@ import { ValidationError } from 'class-validator'
 
 import { benefitTransform } from './benefit'
 
+
 function validationFailure(errs: ValidationError[], property: string, constraint: string) {
   return errs.find(err => err.property === property).constraints[constraint]
 }
@@ -27,5 +28,41 @@ describe('benefitTransform', () => {
       })
       .then(done)
   });
+
+  it('requires several fields to not be empty', done => {
+
+    benefitTransform({ $key: "", description: "", icon: "", oppKey: "", title: "", type: "FoodDrink" })
+      .then(() => {
+        expect(false).toBeTruthy()
+      })
+      .catch((errs: ValidationError[]) => {
+        expect(validationFailure(errs, '$key', 'isNotEmpty')).toBeTruthy()
+        expect(validationFailure(errs, 'oppKey', 'isNotEmpty')).toBeTruthy()
+        expect(validationFailure(errs, 'title', 'isNotEmpty')).toBeTruthy()
+        expect(validationFailure(errs, 'description', 'isNotEmpty')).toBeTruthy()
+      })
+      .then(done)
+  });
+
+  it('requires project type to be Enum', done => {
+
+
+    benefitTransform({
+      $key: "1",
+      description: "Benefit",
+      icon: "",
+      oppKey: "",
+      title: "",
+      type: "NotEnum"
+    })
+      .then(() => {
+        expect(false).toBeTruthy()
+      })
+      .catch((errs: ValidationError[]) => {
+        expect(validationFailure(errs, 'type', 'isEnum')).toBeTruthy()
+      })
+      .then(done)
+  });
+
 
 });
