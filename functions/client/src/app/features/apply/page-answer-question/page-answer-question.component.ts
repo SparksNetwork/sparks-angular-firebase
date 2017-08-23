d8859253a204d833f8cbfd840ae190057a68427f
Szabo Tamas <tamass@imprezzio.com>
2017-08-23 15:57:00 +0300

    added form for question

--- functions/client/src/app/features/apply/page-answer-question/page-answer-question.component.ts
+++ functions/client/src/app/features/apply/page-answer-question/page-answer-question.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApplicationActionService } from "../../../core/sndomain/application";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Application, ApplicationStatus } from "../../../../../../universal/domain/application";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'apply-page-answer-question',
@@ -10,12 +11,18 @@ import { Application, ApplicationStatus } from "../../../../../../universal/doma
 export class PageAnswerQuestionComponent implements OnInit {
   private oppKey: string;
   private applicationKey: string;
+  public answerForm: FormGroup;
 
   constructor(
     public applicationAction: ApplicationActionService,
     public route: ActivatedRoute,
-    public router: Router
-  ) { }
+    public router: Router,
+    public builder: FormBuilder
+  ) {
+    this.answerForm = builder.group({
+      answer: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit() {
     this.oppKey = this.route.snapshot.params["oppKey"];
