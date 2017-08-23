30a28131a6f89c70e8d4e56883fe809c3e90c2cb
Emanuel Virca <emanuelv@imprezzio.com>
2017-08-23 17:05:44 +0300

    get opportunities by projectId

--- functions/client/src/app/core/sndomain/application/application-query.service.ts
+++ functions/client/src/app/core/sndomain/application/application-query.service.ts
@@ -1,12 +1,21 @@
 import { EventEmitter, Injectable } from "@angular/core";
 import { AngularFireDatabase } from "angularfire2/database";
-import { ApplicationCollection } from "../../../../../../universal/domain/application";
+
+import { ApplicationCollection, applicationsTransform } from "../../../../../../universal/domain/application";
+import { SorryService } from "../../sorry/sorry.service";
+import { list } from "../../../../../../lib/firebase-angular-observables/list";
 
 @Injectable()
 export class ApplicationQueryService extends ApplicationCollection {
-    constructor(
-        public af: AngularFireDatabase
-      ) {
-        super(af.database)
-      }
+  constructor(
+    public af: AngularFireDatabase,
+    public sorry: SorryService,
+  ) {
+    super(af.database)
+  }
+
+  public byProfileKey(profileKey: string) {
+    return list(this.by('profileKey', profileKey))
+      .switchMap(this.sorry.intercept(applicationsTransform));
+  }
 }
