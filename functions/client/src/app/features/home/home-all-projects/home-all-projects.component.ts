b116b520d2ec2ec933745b838f0502ffaab4a8fd
Emanuel Virca <emanuelv@imprezzio.com>
2017-08-23 17:16:06 +0300

    display incomplete status for listed projects

--- functions/client/src/app/features/home/home-all-projects/home-all-projects.component.ts
+++ functions/client/src/app/features/home/home-all-projects/home-all-projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Project } from "../../../../../../universal/domain/project";
+import { Application, ApplicationStatus } from "../../../../../../universal/domain/application";
 
 @Component({
     selector: 'home-all-projects',
@@ -8,7 +9,25 @@ import { Project } from "../../../../../../universal/domain/project";
 
 export class HomeAllProjectsComponent {
     @Input() projects: Project[];
+    @Input() applications: Application[];
 
-    constructor() { }
+    constructor() {
+
+    }
+
+    getStatusByProjectKey(projectKey: string) {
+        if (!this.applications || !this.applications.length || !projectKey) return;
+
+        const application = this.applications.find(application => application.projectKey == projectKey);
+        if (application) return this.getStatusDisplay(application.status);
+    }
+
+    getStatusDisplay(status: ApplicationStatus) {
+        switch (status) {
+            case ApplicationStatus.Incomplete:
+                return "Incomplete";
+            default: return null;
+        }
+    }
 
 }
