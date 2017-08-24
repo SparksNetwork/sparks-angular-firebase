2b18c18c055938816e60a4247a53fd025bba07e6
Szabo Tamas <tamass@imprezzio.com>
2017-08-24 14:19:27 +0300

    inject sorry service in application resolver

--- functions/client/src/app/core/sndomain/application/resolve-application-by-key.service.ts
+++ functions/client/src/app/core/sndomain/application/resolve-application-by-key.service.ts
@@ -9,18 +9,20 @@ import { Observable } from "rxjs/Observable";
 import { obj } from '../../../../../../lib/firebase-angular-observables'
 import { ApplicationQueryService } from "./application-query.service";
 import { Application, applicationTransform } from "../../../../../../universal/domain/application";
+import { SorryService } from '../../sorry'
 
 @Injectable()
 export class ResolveApplicationByKey implements Resolve<any> {
 
   constructor(
+    public sorry: SorryService,
     public query: ApplicationQueryService
   ) { }
 
   public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Observable<Application | void>> {
     const applicationKey = route.paramMap.get('applicationKey')
     const application = obj(this.query.one(applicationKey))
-      .mergeMap(applicationTransform)
+      .switchMap(this.sorry.intercept(applicationTransform))
 
     return application
       .map(() => application)
