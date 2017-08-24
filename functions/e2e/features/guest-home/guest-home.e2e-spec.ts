f2994fbeeb1ec64415ca7de2399241134ee396f9
crinelap <crinelap@imprezzio.com>
2017-08-24 15:44:47 +0300

    Finishing tests for guest home page

--- functions/e2e/features/guest-home/guest-home.e2e-spec.ts
+++ functions/e2e/features/guest-home/guest-home.e2e-spec.ts
@@ -1,7 +1,9 @@
 import 'jasmine' // to clear lint errors
 import { browser, element, by, ExpectedConditions } from 'protractor'
-import { setData, updateData, setUsers } from '../../firebase'
-import { GuestHomePage } from "../../po/guest-home.po";
+import { setData, updateData, setUsers, signOut } from '../../firebase'
+import { GuestHomePage } from '../../po/guest-home.po'
+import { DatePipe } from '@angular/common'
+import { ProjectMultiOppPage } from '../../po/project.multi-opp.po';
 
 const waitTimeout = 5000;
 
@@ -10,27 +12,44 @@ describe('Guest home page: user is not logged in', () => {
     const projects = fullyLoaded['project']
     let page: GuestHomePage;
 
+    //helper functions
+    function validatePropertyAgainstDatabase(propertyName: string, displayedValue: string): boolean {
+        for (let key in projects) {
+            if (projects[key][propertyName].toString() === displayedValue) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function getDisplayedProjectKey(url: string): string {
+        let splittedUrl = url.split('/');
+        return splittedUrl[splittedUrl.length - 1];
+    }
+
     beforeAll(done => {
         browser.waitForAngularEnabled(false)
         setUsers()
             .then(() => setData('/', fullyLoaded))
             .then(() => page.navigateTo())
+            //user is not logged in
+            .then(signOut)
             .then(done)
         page = new GuestHomePage();
     })
 
 
-
     describe('exploring the project page', () => {
 
-        function validatePropertyAgainstDatabase(propertyName: string, displayedValue: string): boolean {
-            for (let key in projects) {
-                if (projects[key][propertyName].toString() == displayedValue) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        it('It should display a welcome message for the guest', () => {
+
+            let welcomeMessage = page.getWelcomeMessage()
+            browser.wait(ExpectedConditions.presenceOf(welcomeMessage),
+                waitTimeout, 'Welcome message was not present')
+
+            welcomeMessage.getText().then(function (str) {
+                expect(str).toContain('Guest')
+            })
+        })
 
         it('It should display all projects', () => {
 
@@ -44,12 +63,12 @@ describe('Guest home page: user is not logged in', () => {
                 isPresent = false;
                 projectLinks.each(function (item) {
                     item.getAttribute('href').then(function (str) {
-                        if (str.indexOf(key.toString()) >= 0) {
+                        if (getDisplayedProjectKey(str) === key.toString()) {
                             isPresent = true;
                         }
                     })
-                }).then(function(){
-                    expect(isPresent).toBe(true);
+                }).then(function () {
+                    expect(isPresent).toBe(true, 'Project with key ' + key.toString() + ' was not present');
                 })
             }
         })
@@ -79,13 +98,95 @@ describe('Guest home page: user is not logged in', () => {
 
         })
 
+        it('Each project should display the location', () => {
 
+            let projectLinks = page.getListOfProjectLinks();
+            browser.wait(ExpectedConditions.presenceOf(projectLinks.first()),
+                waitTimeout, 'First link was not present')
+
+            let projectIndex: number = 0;
+            projectLinks.each(function (item) {
+                item.getAttribute('href').then(function (str) {
+                    let projectLocation = page.getProjectLocation(projectIndex)
+                    let projectKey = getDisplayedProjectKey(str)
+                    projectLocation.getText().then(function (str) {
+                        expect(str).toContain(projects[projectKey]['location']['name'], 'Location name was not displayed')
+                        expect(str).toContain(projects[projectKey]['location']['city'], 'City was not displayed')
+                    })
+                }).then(function () {
+                    projectIndex++;
+                })
+            })
+        })
+
+        it('Each project should display the date', () => {
+
+            let projectLinks = page.getListOfProjectLinks();
+            browser.wait(ExpectedConditions.presenceOf(projectLinks.first()),
+                waitTimeout, 'First link was not present')
+
+            let datePipe: DatePipe = new DatePipe('longDate');
+            let projectIndex: number = 0;
+            projectLinks.each(function (item) {
+                item.getAttribute('href').then(function (str) {
+                    let projectDate = page.getProjectDate(projectIndex)
+                    let projectKey = getDisplayedProjectKey(str)
+                    projectDate.getText().then(function (str) {
+                        expect(str).toContain(datePipe.transform(projects[projectKey]['startDateTime'], 'longDate'),
+                            'Start date was not correct displayed')
+                        if (projects[projectKey]['endDateTime']) {
+                            expect(str).toContain(datePipe.transform(projects[projectKey]['endDateTime'], 'longDate'),
+                                'End date was not correct displayed')
+                        }
+                    })
+                }).then(function () {
+                    projectIndex++;
+                })
+            })
+        })
+
+        it('Each project card should open the project page and see the title', () => {
+
+            let projectLinks = page.getListOfProjectLinks();
+            browser.wait(ExpectedConditions.presenceOf(projectLinks.first()),
+                waitTimeout, 'First link was not present')
+
+            projectLinks.count().then(function (projectsNo) {
+                for (let i = 0; i < projectsNo; i++) {
+                    let projectKey: string;
+                    let currentProjectLink = page.getProjectLink(i)
+                    browser.wait(ExpectedConditions.presenceOf(currentProjectLink),
+                        waitTimeout, 'Link ' + i + ' was not present').then(function () {
+                            currentProjectLink.getAttribute('href').then(function (str) {
+                                page.getProjectTitle(currentProjectLink).click().then(function () {
+                                    let projectKey = getDisplayedProjectKey(str)
+                                    browser.wait(ExpectedConditions.urlContains('/project/' + projectKey),
+                                        waitTimeout, 'Link to project ' + projectKey + ' did not open')
+                                        .then(function () {
+                                            let projectPage = new ProjectMultiOppPage()
+                                            let projectTitle = projectPage.getProjectTitleElement();
+                                            browser.wait(ExpectedConditions.presenceOf(projectTitle),
+                                                waitTimeout, 'Title of project ' + projectKey + ' was not present')
+                                                .then(function () {
+                                                    projectTitle.getText().then(function (title) {
+                                                        expect(title).toEqual(projects[projectKey]['title'], 'Title of project ' + projectKey + ' was not correct')
+                                                        page.navigateTo()
+                                                    })
+
+                                                })
+
+                                        })
+
+                                })
+
+                            })
+                        })
+
+                }
+            })
+        })
     })
-
-
-
-
-
-
 })
+
+
 
--- functions/e2e/po/guest-home.po.ts
+++ functions/e2e/po/guest-home.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class GuestHomePage {
 
@@ -6,16 +6,34 @@ export class GuestHomePage {
     return browser.get('/');
   }
 
+  getWelcomeMessage(){
+    return element(by.css('user-header div.profile-hero div.profile-hero-content h2'))
+  }
   getListOfProjectLinks() {
     return element.all(by.css('home-all-projects a'));
+  }
+
+  getProjectLink(projectIndex:number){
+    return element.all(by.css('home-all-projects a')).get(projectIndex);
   }
 
   getAllProjectTitles() {
     return element.all(by.css('home-all-projects a h3'));
   }
 
+  getProjectTitle(el:ElementFinder) {
+    return el.element(by.css('h3'))
+  }
+
   getAllProjectMaxKarmaPoints() {
     return element.all(by.css('home-all-projects a div.event-karma div.event-karma-count'));
   }
 
+  getProjectLocation(projectIndex: number) {
+    return element.all(by.css('home-all-projects a ')).get(projectIndex).element(by.css('div.event-location'));
+  }
+
+  getProjectDate(projectIndex: number) {
+    return element.all(by.css('home-all-projects a ')).get(projectIndex).element(by.css('div.event-date'));
+  }
 }
