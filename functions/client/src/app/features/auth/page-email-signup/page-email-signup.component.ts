3f19ac63a4222c2fac032fb7bf85e95b4b1383fe
Emanuel Virca <emanuelv@imprezzio.com>
2017-08-29 16:19:47 +0300

    show validation errors only after submitting email-password-form

--- functions/client/src/app/features/auth/page-email-signup/page-email-signup.component.ts
+++ functions/client/src/app/features/auth/page-email-signup/page-email-signup.component.ts
@@ -1,21 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
 
-import { FormEmailPasswordComponent } from "../form-email-password/form-email-password.component";
 import { AuthService } from "../../../core/snauth/auth/auth.service";
 
 @Component({
   selector: 'app-page-email-signup',
-  templateUrl: './page-email-signup.component.html', 
+  templateUrl: './page-email-signup.component.html',
 })
 export class PageEmailSignupComponent {
-  @ViewChild(FormEmailPasswordComponent) public epForm: FormEmailPasswordComponent
 
-  constructor(private auth: AuthService) { }  
+  constructor(private auth: AuthService) { }
 
-  public createWithEmailAndPassword() {
-    this.auth.createWithEmailAndPassword(
-      this.epForm.credentialsForm.value.email,
-      this.epForm.credentialsForm.value.password
-    )
+  public createWithEmailAndPassword(event) {
+    if(!event) return;
+    
+    this.auth.createWithEmailAndPassword(event.email, event.password)
   }
 }
--- functions/client/src/app/features/auth/page-signin/page-signin.component.ts
+++ functions/client/src/app/features/auth/page-signin/page-signin.component.ts
@@ -1,22 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
 
 import { AuthService } from "../../../core/snauth/auth/auth.service";
-import { FormEmailPasswordComponent } from "../form-email-password/form-email-password.component";
 
 @Component({
   selector: 'auth-page-signin',
   templateUrl: 'page-signin.component.html'
 })
 
-export class PageSigninComponent {
-  @ViewChild(FormEmailPasswordComponent) public epForm: FormEmailPasswordComponent
+export class PageSigninComponent {  
 
   constructor(private auth: AuthService) { }
 
-  signInWithEmailAndPassword() {
-    this.auth.signInWithEmailAndPassword(
-      this.epForm.credentialsForm.value.email,
-      this.epForm.credentialsForm.value.password
-    )
+  public signInWithEmailAndPassword(event) {
+    this.auth.signInWithEmailAndPassword(event.email, event.password);
   }
 }
