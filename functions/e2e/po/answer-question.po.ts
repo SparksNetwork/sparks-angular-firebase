65d934d8f2dac5bb13e95b58e91df6ef22aba12b
crinelap <crinelap@imprezzio.com>
2017-08-29 16:40:39 +0300

    Tests for answer question

--- functions/e2e/po/answer-question.po.ts
+++ functions/e2e/po/answer-question.po.ts
@@ -1,5 +1,17 @@
 import { browser, by, element } from 'protractor';
 
-export class AnswerPage {
+export class AnswerQuestionPage {
+
+    getNextButton(){
+        return element(by.className('btn btn-block btn-default'))
+    }
+
+    getAnswer(){
+        return element(by.css('textarea'))
+    }
+
+    getQuestion(){
+        return element(by.className('card-text'))
+    }
     
 }
