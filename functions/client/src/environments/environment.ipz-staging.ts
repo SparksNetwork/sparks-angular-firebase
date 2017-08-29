b8cdb6e03dc6687894831edcc29eda42fad9908e
ildikod <ildikod@imprezzio.com>
2017-08-29 16:16:31 +0300

    Config for ipz-staging environment

---
+++ functions/client/src/environments/environment.ipz-staging.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiRoot: 'https://us-central1-sparks-ipz-staging.cloudfunctions.net/api',
+  firebase: {
+    apiKey: "AIzaSyBZMwYQtH84sV1ilI-Jw7zbfEjtKabf3LE",
+    authDomain: "sparks-ipz-staging.firebaseapp.com",
+    databaseURL: "https://sparks-ipz-staging.firebaseio.com",
+    projectId: "sparks-ipz-staging",
+    storageBucket: "sparks-ipz-staging.appspot.com",
+    messagingSenderId: "42777463821"
+  }
+};
