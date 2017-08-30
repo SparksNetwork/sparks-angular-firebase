9668bde3d5f9eaec72db327167be0030ce2fac85
monikaboldizsar <monikaboldizsar@imprezzio.com>
2017-08-30 16:23:04 +0300

    change header section for opp page + added styles for header and opp-head + updated test
    SparksNetw-109395  [UI Design] feat/opp-page-styles

---
+++ functions/client/src/app/features/auth/header-auth/header-auth.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header-auth',
+  templateUrl: './header-auth.component.html'
+})
+export class HeaderAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
