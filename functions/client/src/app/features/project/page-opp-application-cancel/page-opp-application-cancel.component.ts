f5155f93f08096f6e19301939bca9e40e4d6392d
Szabo Tamas <tamass@imprezzio.com>
2017-08-31 15:10:29 +0300

    generated component

---
+++ functions/client/src/app/features/project/page-opp-application-cancel/page-opp-application-cancel.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'page-opp-application-cancel.component.html'
+})
+
+export class PageOppApplicationComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
