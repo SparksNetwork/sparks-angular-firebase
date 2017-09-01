5a536e13a4f74bcc673c59495fb04e884c9e2d37
ildikod <ildikod@imprezzio.com>
2017-09-01 18:29:05 +0300

    Display opp data

--- functions/client/src/app/features/apply/page-payment-confirmation/page-payment-confirmation.component.ts
+++ functions/client/src/app/features/apply/page-payment-confirmation/page-payment-confirmation.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseObjectObservable } from "angularfire2/database";
-import { Project } from "../../../../../../universal/domain/project";
 import { ActivatedRoute } from "@angular/router";
+
+import { Project } from "../../../../../../universal/domain/project";
+import { Opp } from "../../../../../../universal/domain/opp";
+import { Profile } from "../../../../../../universal/domain/profile";
 
 @Component({
   templateUrl: './page-payment-confirmation.component.html'
@@ -9,11 +12,13 @@ import { ActivatedRoute } from "@angular/router";
 export class PagePaymentConfirmationComponent implements OnInit {
 
   public project: FirebaseObjectObservable<Project>;
+  public opp: FirebaseObjectObservable<Opp>;
   
   constructor(
     public route: ActivatedRoute
   ) { 
     this.project = this.route.snapshot.data['project'];
+    this.opp = this.route.parent.parent.snapshot.data['opp'];
   }
 
   ngOnInit() {
