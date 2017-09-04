5c6319f4024b1efc42a6812660e58ff8d4f250cb
ildikod <ildikod@imprezzio.com>
2017-09-04 15:55:27 +0300

    Fix lint errors

--- functions/client/src/app/features/apply/apply-routing.module.ts
+++ functions/client/src/app/features/apply/apply-routing.module.ts
@@ -6,19 +6,19 @@ import {
 } from '../../core/sndomain/opp'
 
 import { RequireAuth } from '../../core/snauth/require-auth/require-auth.service'
-import { RequireEmailVerification } from "../../core/snauth/require-email-verification/require-email-verification.service";
-import { PageCompleteProfileComponent } from "./page-complete-profile/page-complete-profile.component";
-import { ResolveTeamByOppKey } from "../../core/sndomain/team/resolve-team-by-opp-key.service";
-import { PageOppTeamsComponent } from "./page-opp-teams/page-opp-teams.component";
-import { PageOppTeamComponent } from "./page-opp-team/page-opp-team.component";
+import { RequireEmailVerification } from '../../core/snauth/require-email-verification/require-email-verification.service';
+import { PageCompleteProfileComponent } from './page-complete-profile/page-complete-profile.component';
+import { ResolveTeamByOppKey } from '../../core/sndomain/team/resolve-team-by-opp-key.service';
+import { PageOppTeamsComponent } from './page-opp-teams/page-opp-teams.component';
+import { PageOppTeamComponent } from './page-opp-team/page-opp-team.component';
 import { PageAnswerQuestionComponent } from './page-answer-question/page-answer-question.component'
-import { ResolveTeamByTeamKey } from "./resolve-team-by-team-key/resolve-team-by-team-key.service";
+import { ResolveTeamByTeamKey } from './resolve-team-by-team-key/resolve-team-by-team-key.service';
 import { RequireProfileCompleteService, ResolveProfile } from '../../core/sndomain/profile'
-import { ResolveApplicationTeamByAppKey } from "../../core/sndomain/applicationTeam/resolve-application-team-by-app-key.service";
-import { ResolveApplicationByKey } from "../../core/sndomain/application/resolve-application-by-key.service";
-import { PageReviewDetailComponent } from "./page-review-detail/page-review-detail.component";
-import { PageApplyConfirmationComponent } from "./page-apply-confirmation/page-apply-confirmation.component";
-import { ResolveProjectByOpp } from "../../core/sndomain/project/resolve-project-by-opp.service";
+import { ResolveApplicationTeamByAppKey } from '../../core/sndomain/applicationTeam/resolve-application-team-by-app-key.service';
+import { ResolveApplicationByKey } from '../../core/sndomain/application/resolve-application-by-key.service';
+import { PageReviewDetailComponent } from './page-review-detail/page-review-detail.component';
+import { PageApplyConfirmationComponent } from './page-apply-confirmation/page-apply-confirmation.component';
+import { ResolveProjectByOpp } from '../../core/sndomain/project/resolve-project-by-opp.service';
 import { PagePaymentDetailsComponent } from './page-payment-details/page-payment-details.component';
 import { PagePaymentConfirmationComponent } from './page-payment-confirmation/page-payment-confirmation.component';
 
@@ -88,7 +88,7 @@ const routes: Routes = [
                     },
                     {
                         path: 'edit-profile',
-                        component: PageCompleteProfileComponent                        
+                        component: PageCompleteProfileComponent
                     },
                     {
                         path: 'answer-question',
