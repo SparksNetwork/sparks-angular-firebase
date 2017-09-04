b7eca09dec5a12eeaff87309d4d0ba816d194186
ildikod <ildikod@imprezzio.com>
2017-09-05 01:09:55 +0300

    Fix team query and action service

--- functions/client/src/app/core/sndomain/team/team-action.service.ts
+++ functions/client/src/app/core/sndomain/team/team-action.service.ts
@@ -1,18 +1,19 @@
-// import { Injectable } from '@angular/core'
-// import { Http } from '@angular/http'
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
 
-// import { BaseActionService } from '../../../../../../lib/firebase-universal/client'
+import { environment } from '../../../../environments/environment';
+import { BaseActionService } from '../../../../../../lib/firebase-universal/client'
 
-// import { TeamQueryService } from './team-query.service'
+import { TeamQueryService } from './team-query.service'
 
-// @Injectable()
-// export class TeamActionService extends BaseActionService {
+@Injectable()
+export class TeamActionService extends BaseActionService {
 
-//   constructor(
-//     public query: TeamQueryService,
-//     public http: Http,
-//   ) {
-//     super(query.paths, http)
-//   }
+  constructor(
+    public query: TeamQueryService,
+    public http: Http,
+  ) {
+    super(environment.apiRoot, query.paths.api, http)
+  }
 
-// }
+}
