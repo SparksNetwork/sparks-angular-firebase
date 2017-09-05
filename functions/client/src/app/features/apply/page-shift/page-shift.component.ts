269ceddcf683f86298036b296a4935bea0fe6333
ildikod <ildikod@imprezzio.com>
2017-09-05 17:56:47 +0300

    Fix action bar type

--- functions/client/src/app/features/apply/page-shift/page-shift.component.ts
+++ functions/client/src/app/features/apply/page-shift/page-shift.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import { Project } from "../../../../../../universal/domain/project";
+import { ActivatedRoute } from '@angular/router';
+import { Project } from '../../../../../../universal/domain/project';
+import { ActionBarType } from '../../../shared/snui/action-bar/action-bar.component';
 
 @Component({
   templateUrl: './page-shift.component.html',
 })
 export class PageShiftComponent implements OnInit {
 
+  public actionBarType = ActionBarType;
   public project: Project;
 
   constructor(private route: ActivatedRoute) {
-    this.route.snapshot.data["project"].subscribe(project => this.project = project)
+    this.route.snapshot.data['project'].subscribe(project => this.project = project)
   }
 
   ngOnInit() {
