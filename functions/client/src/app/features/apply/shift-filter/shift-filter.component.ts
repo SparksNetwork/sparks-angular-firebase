71b06d24c9038a5b937105a7b214126877fa9513
ildikod <ildikod@imprezzio.com>
2017-09-05 17:09:46 +0300

    Add shift filter component

---
+++ functions/client/src/app/features/apply/shift-filter/shift-filter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apply-shift-filter',
+  templateUrl: './shift-filter.component.html'
+})
+export class ShiftFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
