678ca1df55d51461dd23c4d86893f038cd67740d
Emanuel Virca <emanuelv@imprezzio.com>
2017-09-06 16:16:36 +0300

    Select shift

--- functions/client/src/app/features/apply/shift-list/shift-list.component.ts
+++ functions/client/src/app/features/apply/shift-list/shift-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Shift } from '../../../../../../universal/domain/shift';
+import { ApplicationShift } from '../../../../../../universal/domain/applicationShift';
+
+import { ApplicationShiftActionService } from '../../../core/sndomain/applicationShift/application-shift-action.service';
 
 @Component({
   selector: 'apply-shift-list',
@@ -9,11 +13,22 @@ import { Shift } from '../../../../../../universal/domain/shift';
 export class ShiftListComponent implements OnInit {
   @Input() shifts: Shift[]
 
-  constructor(route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    public applicationShiftAction: ApplicationShiftActionService
+  ) {
     this.shifts = route.snapshot.data['shift'];
   }
 
   ngOnInit() {
   }
 
+  public select(key: string) {
+    const applicationShift = new ApplicationShift();
+    applicationShift.appKey = this.route.snapshot.parent.paramMap.get('applicationKey');
+    applicationShift.shiftKey = key;
+    applicationShift.joinedOn = new Date().toISOString();
+
+    this.applicationShiftAction.create(applicationShift).subscribe((a) => { console.log(a) });
+  }
 }
