b11a5e48d3357858f9fbce1b2b71a91edd213e74
ildikod <ildikod@imprezzio.com>
2017-09-06 12:59:08 +0300

    Use unique team title values for the filter

--- functions/client/src/app/features/apply/shift-filter/shift-filter.component.ts
+++ functions/client/src/app/features/apply/shift-filter/shift-filter.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';;
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Shift } from '../../../../../../universal/domain/shift';
+import { Team } from '../../../../../../universal/domain/team';
 
 @Component({
   selector: 'apply-shift-filter',
   templateUrl: './shift-filter.component.html'
 })
-export class ShiftFilterComponent implements OnInit {
-
-  @Input() shifts: Shift[]
+export class ShiftFilterComponent implements OnChanges {
+  @Input() private shifts: Shift[]
+  private teamFilter: ITeamFilter[]
 
   constructor(
   ) { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.shifts) {
+      const tempTeams = this.shifts.map((data) => <ITeamFilter>({ 'teamKey': data.teamKey, 'teamTitle': data.teamTitle }));
+      this.teamFilter = tempTeams.filter((item, pos) => tempTeams.map(t => t.teamKey).indexOf(item.teamKey) === pos)
+        .sort((t1, t2) => t1.teamTitle > t2.teamTitle ? 1 : (t2.teamTitle > t1.teamTitle ? -1 : 0));
+    }
   }
+}
 
+interface ITeamFilter {
+  teamKey: string;
+  teamTitle: string;
 }
