a55d5ff0d45fde3486bc3c72ea03422ab3ebdbf0
crinelap <crinelap@imprezzio.com>
2017-09-08 12:34:49 +0300

    Adding test for diffrent types of users

--- functions/e2e/features/apply/answer-team-question.e2e-spec.ts
+++ functions/e2e/features/apply/answer-team-question.e2e-spec.ts
@@ -1,13 +1,14 @@
 import 'jasmine'
 import { AnswerTeamQuestionPage } from '../../po/apply.answer-team-question.po';
 import { browser, ExpectedConditions } from 'protractor/built';
-import { USER_VERIFIED_PROFILE } from '../../fixtures/users';
-import { LC_INCOMPLETE_APP } from "../../fixtures/applications/application";
+import { USER_VERIFIED_PROFILE, USER_VERIFIED_NO_PROFILE, USER_NOT_VERIFIED } from '../../fixtures/users';
+import { LC_INCOMPLETE_APP, LC_INCOMPLETE_APP_USER_VER_NO_PROFILE, LC_INCOMPLETE_APP_USER_VER_ONLY_FNAME, LC_INCOMPLETE_APP_USER_NOT_VER } from "../../fixtures/applications/application";
 import { ApplicationStages } from "../../fixtures/applications/application-stages";
+import { USER_VERIFIED_LNAME } from "../../fixtures/users-partial-profile";
 
 
 const waitTimeout = 7000
-describe('Apply-Choosing-Teams: verified user with complete profile information', () => {
+describe('Apply-Choose-Teams: user must have complete and verified profile before join a team', () => {
     let answerTeamQuestionPage: AnswerTeamQuestionPage
 
     const fullyLoaded = require('../../fixtures/fully-loaded.json')
@@ -16,48 +17,105 @@ describe('Apply-Choosing-Teams: verified user with complete profile information'
         answerTeamQuestionPage = new AnswerTeamQuestionPage()
         browser.waitForAngularEnabled(false).then(done)
     })
+    describe('User with verified and complete profile', () => {
 
-    describe('Checking Answer-team-question page', () => {
+        describe('Checking Answer-team-question page', () => {
+            beforeAll(done => {
+                ApplicationStages.userWithApplication(USER_VERIFIED_PROFILE, LC_INCOMPLETE_APP)
+                    .then(() => browser.get('/apply/LC1/application/LC_INCOMPLETE_APP/teams/LC1'))
+                    .then(done)
+            })
+
+
+            it('it should display the question', function () {
+                browser.wait(ExpectedConditions.and(
+                    ExpectedConditions.urlContains('/apply/LC1/application/'),
+                    ExpectedConditions.urlContains('/teams/LC1')),
+                    waitTimeout, 'User was not taken to Answer-team-question Page').then(function () {
+                        let question = answerTeamQuestionPage.getQuestion()
+                        browser.wait(ExpectedConditions.presenceOf(question),
+                            waitTimeout, 'The text of the question was not present')
+                        question.getText().then((str) => {
+                            browser.getCurrentUrl().then((url) => {
+                                let oppKey: string = GetOppKey(url)
+                                let teamKey: string = GetTeamKey(url)
+
+                                expect(str).toMatch(fullyLoaded['oppAllowedTeam'][oppKey + '-' + teamKey]['team']['question'],
+                                    'The text of the question was not correct')
+                            })
+                        })
+                    })
+            })
+
+            it('join button is clickable only if the answer field is not empty ', function () {
+                let joinButton = answerTeamQuestionPage.getJoinTeamButton()
+                browser.wait(ExpectedConditions.presenceOf(joinButton),
+                    waitTimeout, 'Join button was not present')
+                browser.wait(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(joinButton)),
+                    waitTimeout, 'Join button was clickable when answer field was empty')
+                answerTeamQuestionPage.getAnswer().sendKeys('Answer is 42')
+                browser.wait(ExpectedConditions.elementToBeClickable(joinButton),
+                    waitTimeout, 'Join button was not clickable when answer was written')
+                expect(true).toBeTruthy()
+            });
+
+        })
+
+
+        describe('User verified and no profile', () => {
+
+            beforeAll(done => {
+                ApplicationStages.userWithApplication(USER_VERIFIED_NO_PROFILE, LC_INCOMPLETE_APP_USER_VER_NO_PROFILE)
+                    .then(() => browser.get('/apply/LC1/application/LC_INCOMPLETE_APP_USER_VER_NO_PROFILE/teams/LC1'))
+                    .then(done)
+            })
+
+            it('It should be redirected to Complete-profile page', function () {
+                browser.wait(ExpectedConditions.urlContains('/apply/LC1/complete-profile'),
+                    waitTimeout, 'User was not taken to Complete-profile Page').then(function () {
+                        expect(true).toBeTruthy()
+                    })
+            })
+
+        })
+    })
+
+    describe('User verified and only legal name completed', () => {
+
         beforeAll(done => {
-            ApplicationStages.userWithApplication(USER_VERIFIED_PROFILE, LC_INCOMPLETE_APP)
-                .then(() => browser.get('/apply/LC1/application/LC_INCOMPLETE_APP/teams/LC1'))
+            ApplicationStages.userWithApplication(USER_VERIFIED_LNAME, LC_INCOMPLETE_APP_USER_VER_ONLY_FNAME)
+                .then(() => browser.get('/apply/LC1/application/LC_INCOMPLETE_APP_USER_VER_ONLY_FNAM/teams/LC1'))
                 .then(done)
         })
 
-
-        it('it should display the question', function () {
-            browser.wait(ExpectedConditions.and(
-                ExpectedConditions.urlContains('/apply/LC1/application/'),
-                ExpectedConditions.urlContains('/teams/LC1')),
-                waitTimeout, 'User was not taken to Answer-team-question Page').then(function () {
-                    let question = answerTeamQuestionPage.getQuestion()
-                    browser.wait(ExpectedConditions.presenceOf(question),
-                        waitTimeout, 'The text of the question was not present')
-                    question.getText().then((str) => {
-                        browser.getCurrentUrl().then((url) => {
-                            let oppKey: string = GetOppKey(url)
-                            let teamKey: string = GetTeamKey(url)
-
-                            expect(str).toMatch(fullyLoaded['oppAllowedTeam'][oppKey + '-' + teamKey]['team']['question'],
-                                'The text of the question was not correct')
-                        })
-                    })
+        it('It should be redirected to Complete-profile page', function () {
+            browser.wait(ExpectedConditions.urlContains('/apply/LC1/complete-profile'),
+                waitTimeout, 'User was not taken to Complete-profile Page').then(function () {
+                    expect(true).toBeTruthy()
                 })
         })
 
-        it('join button is clickable only if the answer field is not empty ', function () {
-            let joinButton = answerTeamQuestionPage.getJoinTeamButton()
-            browser.wait(ExpectedConditions.presenceOf(joinButton),
-                waitTimeout, 'Join button was not present')
-            browser.wait(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(joinButton)),
-                waitTimeout, 'Join button was clickable when answer field was empty')
-            answerTeamQuestionPage.getAnswer().sendKeys('Answer is 42')
-            browser.wait(ExpectedConditions.elementToBeClickable(joinButton),
-                waitTimeout, 'Join button was not clickable when answer was written')
-            expect(true).toBeTruthy()
-        });
+    })
+
+
+    describe('User not verified', () => {
+
+        beforeAll(done => {
+            ApplicationStages.userWithApplication(USER_NOT_VERIFIED, LC_INCOMPLETE_APP_USER_NOT_VER)
+                .then(() => browser.get('/apply/LC1/application/LC_INCOMPLETE_APP_USER_NOT_VER/teams/LC1'))
+                .then(done)
+        })
+
+        it('It should be redirected to Email-not-verified page', function () {
+            browser.wait(ExpectedConditions.urlContains('/auth/email-not-verified'),
+                waitTimeout, 'User was not taken to Email-not-verified Page').then(function () {
+                    expect(true).toBeTruthy()
+                })
+        })
 
     })
+   
+
     //helper functions
     function GetTeamKey(url: string) {
         let splittedUrl = url.split('/');
