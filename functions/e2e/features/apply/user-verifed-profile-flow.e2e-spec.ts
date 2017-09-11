bc0acad3e2a3797f35636cf1dda769f2eceedd06
crinelap <crinelap@imprezzio.com>
2017-09-11 17:01:45 +0300

    Adding tests for the apply flow, verified user with profile

---
+++ functions/e2e/features/apply/user-verifed-profile-flow.e2e-spec.ts
@@ -0,0 +1,83 @@
+import 'jasmine'
+import { ProjectSingleOppPage } from '../../po/project.single-opp.po';
+import { AnswerOrganizerQuestionPage } from '../../po/apply.answer-organizer-question.po';
+import { PickTeamPage } from '../../po/apply.choose.team.po';
+import { AnswerTeamQuestionPage } from '../../po/apply.answer-team-question.po';
+import { browser, ExpectedConditions } from "protractor/built";
+import { setUsers, setData, signIn, signOut, setUsersWithPartialProfile } from '../../firebase';
+import { USER_VERIFIED_PROFILE } from "../../fixtures/users";
+import { confirmPage } from "../helper-functions/navigation/navigation-functions";
+import { joinATeam } from "../helper-functions/choose-teams/choose-teams-functions";
+import { ReviewApplicationDetailsPage } from "../../po/apply.review-application-details.po";
+
+describe('Apply-Single-Team-Flow: verified user with complete profile information', () => {
+    let KPCprojectPage: ProjectSingleOppPage
+    let answerOrganizerQuestionPage: AnswerOrganizerQuestionPage
+    let pickTeamPage: PickTeamPage
+    let answerTeamQuestionPage: AnswerTeamQuestionPage
+    let reviewApplicationDetailsPage: ReviewApplicationDetailsPage
+
+    const fullyLoaded = require('../../fixtures/fully-loaded.json')
+    const waitTimeout = 5000
+
+    beforeAll(done => {
+        KPCprojectPage = new ProjectSingleOppPage();
+        answerOrganizerQuestionPage = new AnswerOrganizerQuestionPage();
+        pickTeamPage = new PickTeamPage()
+        answerTeamQuestionPage = new AnswerTeamQuestionPage()
+        reviewApplicationDetailsPage = new ReviewApplicationDetailsPage()
+        browser.waitForAngularEnabled(false)
+        setUsers()
+            .then(() => setUsersWithPartialProfile())
+            .then(() => setData('/', fullyLoaded))
+            .then(() => browser.get('/'))
+            .then(() => signOut())
+            .then(() => signIn(USER_VERIFIED_PROFILE.email, USER_VERIFIED_PROFILE.password))
+            .then(done)
+    });
+
+
+
+    it('It should be able to choose KPC Event, answer organizer question, ' +
+        'select the available team, review the application details and submit ', function () {
+
+            KPCprojectPage.navigateTo()
+                .then(() =>
+                    browser.wait(ExpectedConditions.elementToBeClickable(KPCprojectPage.getJoinButton()),
+                        waitTimeout, 'Join button was not present'))
+                .then(() => KPCprojectPage.getJoinButton().click())
+                .then(() =>
+                    confirmPage('/apply/KPC1/answer-question', '', 'Answer-question', 'first', waitTimeout))
+                .then(() =>
+                    confirmPage('/apply/KPC1/application', '/answer-question', 'Answer-organizer-question', 'first', waitTimeout))
+                .then(() => {
+                    browser.wait(ExpectedConditions.presenceOf(answerOrganizerQuestionPage.getNextButton()),
+                        waitTimeout, 'Next button was not present')
+                    answerOrganizerQuestionPage.getAnswer().sendKeys('42')
+                    let next = answerOrganizerQuestionPage.getNextButton()
+                    browser.wait(ExpectedConditions.elementToBeClickable(next),
+                        waitTimeout, 'Next button was not clickable')
+                    return next.click()
+                })
+                .then(() =>
+                    confirmPage('/apply/KPC1/application/', '/teams', 'Pick-teams', 'first', waitTimeout, '/teams/'))
+                .then(() => joinATeam(pickTeamPage, waitTimeout, 'KPC1', answerTeamQuestionPage))
+                .then(() => {
+                    let nextButton = pickTeamPage.getNextButton()
+                    browser.wait(ExpectedConditions.elementToBeClickable(nextButton),
+                        waitTimeout, 'Next button was not clickable when the team was selected')
+                    return nextButton.click()
+                })
+                .then(() => confirmPage('/apply/KPC1/application/', '/review-detail', 'Review-application-details', 'first', waitTimeout))
+                .then(() => {
+                    let nextButton = reviewApplicationDetailsPage.getNextButton()
+                    browser.wait(ExpectedConditions.elementToBeClickable(nextButton),
+                        waitTimeout, 'Next button was not clickable on Review-application-details page')
+                    return nextButton.click()
+                })
+                .then(() => confirmPage('/apply/KPC1/application/', '/apply-cofirmation', 'Apply-cofirmation', 'first', waitTimeout))
+
+        })
+
+
+})
