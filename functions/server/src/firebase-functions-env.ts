0fb5fb67f7adc7d9c190c40a28a174e6163a2a6e
sd <sdebaun74@gmail.com>
2017-09-24 08:59:34 -0700

    cleanup

--- functions/server/src/firebase-functions-env.ts
+++ functions/server/src/firebase-functions-env.ts
@@ -6,17 +6,12 @@ export let functions: {
   database: any,
 } = firebaseFunctions
 
-// let functions = require('firebase-functions')
-
 if (process.env.NODE_ENV !== 'production') {
+  console.log('running functions in local emulator environment with firebase-functions-local')
   const envCode = process.env['ANGULAR_ENV']
   const env = require(`../../client/src/environments/environment.${envCode}.ts`).environment
   functions = require('firebase-functions-local')({
     config: env.firebase,
-    port: 3001,
+    port: 5000,
   })
 }
-console.log('functions', functions)
-// module.exports = functions
-
-
