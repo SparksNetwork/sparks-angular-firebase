ed1b3c925b9db315ef859b3e5f42c2f647decdc9
sd <sdebaun74@gmail.com>
2017-10-07 14:48:15 -0700

    add logging for triggers

--- functions/server/src/triggers.ts
+++ functions/server/src/triggers.ts
@@ -1,17 +1,29 @@
 import 'reflect-metadata';
 import { functions, admin } from './firebase-functions-env'
 import * as firebase from 'firebase'
-// import * as admin from 'firebase-admin'
+import * as functionsTypes from 'firebase-functions'
+
+import { BaseCollection } from '../../lib/firebase-universal/server'
 
 const getKeys = (snap: firebase.database.DataSnapshot) => Object.keys(snap.val())
 
-function denormalizerFromParentChange(childCollection, foreignKey, denormalizedField) {
-  return function(evt) {
-    console.log('denormalizeFromParentChange', childCollection, foreignKey, denormalizedField)
+function denormalizerFromParentChange(
+  parentCollection: BaseCollection,
+  childCollection: BaseCollection,
+  foreignKey: string,
+  denormalizedField: string
+) {
+  return function(evt: functionsTypes.Event<functionsTypes.database.DeltaSnapshot>) {
     const newData = evt.data.val()
     const parentKey = evt.params.key
-    const updateDenormalizedField = key =>
+    const updateDenormalizedField = key => {
+      console.log(
+        'denorm/parent:',
+        `${parentCollection.ref.key}/${parentKey} =>`,
+        `${childCollection.ref.key}/${key}/${denormalizedField}`,
+      )
       childCollection.one(key).child(denormalizedField).set(newData)
+    }
 
     return (childCollection.by(foreignKey, parentKey) as firebase.database.Query)
       .once('value')
@@ -20,10 +32,19 @@ function denormalizerFromParentChange(childCollection, foreignKey, denormalizedF
   }
 }
 
-function denormalizerFromForeignKeyChange(parentCollection, childCollection, denormalizedField) {
-  return function(evt) {
+function denormalizerFromChildForeignKeyChange(
+  parentCollection: BaseCollection,
+  childCollection: BaseCollection,
+  denormalizedField: string
+) {
+  return function(evt: functionsTypes.Event<functionsTypes.database.DeltaSnapshot>) {
     const parentKey = evt.data.val()
     const childKey = evt.params.key
+    console.log(
+      'denorm/child: ',
+      `${childCollection.ref.key}/${childKey}/${denormalizedField} <=`,
+      `${parentCollection.ref.key}/${parentKey}`
+    )
     return parentCollection.one(parentKey)
       .once('value')
       .then(snap => snap.val())
@@ -31,14 +52,20 @@ function denormalizerFromForeignKeyChange(parentCollection, childCollection, den
   }
 }
 
-function denormalizers(parentCollection, childCollection, foreignKey, denormalizedField) {
+function denormalizers(
+  parentCollection: BaseCollection,
+  childCollection: BaseCollection,
+  foreignKey: string,
+  denormalizedField: string,
+) {
   return {
     onParentChange: denormalizerFromParentChange(
+      parentCollection,
       childCollection,
       foreignKey,
       denormalizedField
     ),
-    onChildForeignKeyChange: denormalizerFromForeignKeyChange(
+    onChildForeignKeyChange: denormalizerFromChildForeignKeyChange(
       parentCollection,
       childCollection,
       denormalizedField
@@ -62,7 +89,7 @@ const teamToOATDenormalizers = denormalizers(
 console.log('TRIGGERS: exporting')
 export const teamToOATTeamOnWrite =
   functions.database.ref('/team/{key}')
-    .onWrite(teamToOATDenormalizers.onParentChange)
+    .onUpdate(teamToOATDenormalizers.onParentChange)
 
 export const teamToOATOATOnUpdate =
   functions.database.ref('/oppAllowedTeam/{key}/teamKey')
