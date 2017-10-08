c8e6193f507cf01b42b068a6d78f21051213feb9
sd <sdebaun74@gmail.com>
2017-10-07 19:12:56 -0700

    added cucumber reporter

--- functions/protractor.conf.js
+++ functions/protractor.conf.js
@@ -1,30 +1,40 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-const BeautifulReporter = require('protractor-beautiful-reporter')
+// const { SpecReporter } = require('jasmine-spec-reporter');
+// const BeautifulReporter = require('protractor-beautiful-reporter')
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
+  // specs: [
+  //   './e2e/**/*.e2e-spec.ts'
+  // ],
   capabilities: {
     'browserName': 'chrome',
     'loggingPrefs': {
       'driver': 'WARNING',
       'server': 'WARNING',
       'browser': 'INFO'
+    },
+    metadata: {
+      browser: {
+        name: 'chrome',
+      },
+      device: 'MacBook Pro',
+      platform: {
+        name: 'OSX',
+        version: '10.11.6'
+      }
     }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   // framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function () { }
-  },
+  // jasmineNodeOpts: {
+  //   showColors: true,
+  //   defaultTimeoutInterval: 30000,
+  //   print: function () { }
+  // },
 
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
@@ -36,8 +46,22 @@ exports.config = {
   cucumberOpts: {
     require: [
       'e2e/cucumber/steps/*.ts'
-    ]
+    ],
+    format: 'json:../e2e-report/results.json',
+    strict: true,
   },
+
+  plugins: [
+    {
+      package: 'protractor-multiple-cucumber-html-reporter-plugin',
+      options: {
+        automaticallyGenerateReport: true,
+        openReportInBrowser: true,
+        removeOriginalJsonReportFile: true,
+        removeExistingJsonReportFile: true,
+      }
+    },
+  ],
 
   onPrepare() {
     require('ts-node').register({
