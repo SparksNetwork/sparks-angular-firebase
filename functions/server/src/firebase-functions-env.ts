c967761e9c80ad227cb293fc85a09520f43cbe45
sd <sdebaun74@gmail.com>
2017-10-07 17:38:07 -0700

    cleanup unused code

--- functions/server/src/firebase-functions-env.ts
+++ functions/server/src/firebase-functions-env.ts
@@ -28,41 +28,11 @@ export const initialize = () => {
     })
     console.log('FIREBASE-ADMIN: initialized with local credentials for ', env.firebase.databaseURL)
 
-  } catch (err) {
+  } catch (err) { // must be running in cloud
     console.log('FIREBASE-ENV: running in the cloud')
     console.log('FIREBASE-FUNCTIONS: running in cloud')
     admin.initializeApp(functions.config().firebase)
     console.log('FIREBASE-ADMIN: cloud credentials', functions.config().firebase)
   }
-
-  // try { // this will only work in local environment
-  //   console.log('FIREBASE-FUNCTIONS: trying local emulator environment with firebase-functions-local')
-  //   const envCode = process.env['ANGULAR_ENV']
-  //   const env = require(`../../client/src/environments/environment.${envCode}.ts`).environment
-  //   functions = require('firebase-functions-local')({
-  //     config: env.firebase,
-  //     port: 5000,
-  //   })
-  // } catch (err) {
-  //   console.log('FIREBASE-FUNCTIONS: running in cloud')
-  // }
-
-  // try {
-  //   console.log('FIREBASE-ADMIN: trying local credentials')
-  //   const envCode = process.env['ANGULAR_ENV']
-  //   console.log('FIREBASE-ADMIN: environment:' + envCode)
-  //   console.log(__dirname)
-  //   const serviceAccount = require(`../../../firebaseAdminCredentials.${envCode}.json`)
-  //   const env = require(`../../client/src/environments/environment.${envCode}.ts`).environment
-  //   admin.initializeApp({
-  //     credential: admin.credential.cert(serviceAccount),
-  //     databaseURL: env.firebase.databaseURL
-  //   })
-  //   console.log('FIREBASE-ADMIN: local credentials')
-  // } catch (err) {
-  //   console.log('FIREBASE-ADMIN: could not use local credentials:', err)
-  //   admin.initializeApp(functions.config().firebase)
-  //   console.log('FIREBASE-ADMIN: cloud credentials', functions.config().firebase)
-  // }
 }
 
